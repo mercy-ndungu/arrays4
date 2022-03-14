2f9719cc96071e1811ff9045c4acaa9cf8f7f491
mercy-ndungu <mercyndungukirigo@gmail.com>
2022-03-14 07:58:01 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,33 @@
+fun main(){
+    var y=myFun(arrayOf(3,4,5,6))
+    println(y)
+    var x = namba(arrayOf("Kenya", 36.2, 5.5, false  ))
+    println(x)
+    var z = mySentence(arrayOf('f', 'a', 'e', 'b', 'i', 'c', 'o', 'u', 'z', 'x'))
+    println(z)
+}
+fun myFun(number:Array<Int>):Int{
+    var digit=1
+    number.forEach { numb->
+        digit*=numb
+    }
+    return digit
+}
+fun namba(tally:Array<Any>):Double{
+    var int = 0.0
+    tally.forEach{unit->
+        if(unit is Double || unit is Float){
+            int+=unit.toString().toDouble()
+        }
+    }
+    return int
+}
+fun mySentence(vowels:Array<Char>):Int{
+    var figure = 0
+    vowels.forEach { stmt->
+        if(stmt=='a' || stmt=='e' || stmt=='i' || stmt=='o' || stmt=='u'){
+            figure++
+        }
+    }
+    return figure
+}
